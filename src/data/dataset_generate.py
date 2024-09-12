27dd6f1a529a17383f703967e973bb725bcd460a
Bingcheng Chen <chenbin@chalmers.se>
2024-09-12 13:03:19 +0200

    Add dataset_generate.py for generating information bit datasets

---
+++ src/data/dataset_generate.py
@@ -0,0 +1,21 @@
+# This file is to generatre a dataset of infomaion bits with the size of (1000000, 1, k), where k is the length of the information bits.
+
+import torch
+
+
+def generate_dataset_info_bits(num_samples, k, file_path="data/processed/info_bits.pt"):
+    # generate a torch tensor with the size of (num_samples, 1, k), each element is an integer number between 0 and 1
+    info_bits = torch.randint(0, 2, (num_samples, 1, k))
+    print(info_bits.shape)
+
+    # save the tensor to a file
+    torch.save(info_bits, file_path)
+
+
+if __name__ == "__main__":
+
+    num_samples = 1000
+    k = 64
+    file_path = f"data/processed/val_dataset_info_bits_{num_samples}_1_{k}.pt"
+
+    generate_dataset_info_bits(num_samples, k, file_path)
