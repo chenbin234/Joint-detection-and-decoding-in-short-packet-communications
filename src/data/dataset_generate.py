1cc2fd11de685f316008f39f9443b50df9997705
Bingcheng Chen <chenbin@chalmers.se>
2024-09-13 10:21:21 +0200

    change dtype to float()

--- src/data/dataset_generate.py
+++ src/data/dataset_generate.py
@@ -5,7 +5,7 @@ import torch
 
 def generate_dataset_info_bits(num_samples, k, file_path="data/processed/info_bits.pt"):
     # generate a torch tensor with the size of (num_samples, 1, k), each element is an integer number between 0 and 1
-    info_bits = torch.randint(0, 2, (num_samples, 1, k))
+    info_bits = torch.randint(0, 2, (num_samples, 1, k)).float()
     print(info_bits.shape)
 
     # save the tensor to a file
@@ -14,8 +14,8 @@ def generate_dataset_info_bits(num_samples, k, file_path="data/processed/info_bi
 
 if __name__ == "__main__":
 
-    num_samples = 1000
+    num_samples = 2000
     k = 64
-    file_path = f"data/processed/val_dataset_info_bits_{num_samples}_1_{k}.pt"
+    file_path = f"data/processed/train_dataset_info_bits_{num_samples}_1_{k}.pt"
 
     generate_dataset_info_bits(num_samples, k, file_path)
