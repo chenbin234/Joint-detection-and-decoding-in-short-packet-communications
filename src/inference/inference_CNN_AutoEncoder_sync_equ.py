44de709a5f0a6cac57ab83c7b0f7c3e255c161f2
chenbin <chenbin@chalmers.se>
2024-09-24 14:22:40 +0200

    add new inference scripts

---
+++ src/inference/inference_CNN_AutoEncoder_sync_equ.py
@@ -0,0 +1,125 @@
+"""
+This file is to inference the model with the CNN AutoEncoder architecture
+(Joint Syncronization, Equalization, and Channel Decoding)
+
+"""
+
+# 1. Import Libraries
+
+import sys
+
+sys.path.append("src/")
+import os
+from inference.inference_utils_sync_equ import inference_loop
+from models.CNN_AutoEncoder_sync_equ import CNN_AutoEncoder
+from features.build_pytorch_dataset import InfobitDataset
+from torch.utils.data import DataLoader
+import wandb
+from datetime import datetime
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import random
+import numpy as np
+
+
+def make(config):
+
+    # test dataset
+    test_dataset = InfobitDataset(num_samples=1e6, k=config.k)
+
+    # create test dataloader
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=config.batch_size, shuffle=False
+    )
+
+    # Initialize the model
+    model = CNN_AutoEncoder(
+        M1=config.M1,
+        M2=config.M2,
+        k=config.k,
+        N=config.N,
+        L=config.L,
+        k_mod=config.k_mod,
+        F=config.F,
+        delay_max=config.delay_max,
+        nb=config.nb,
+        N_up=config.N_up,
+        tp=config.tp,
+    )
+
+    return model, test_dataloader
+
+
+def model_pipeline(hyperparameters):
+
+    # tell wandb to get started
+    with wandb.init(
+        project="Joint-detection-and-decoding-in-short-packet-communications",
+        config=hyperparameters,
+    ):
+        # access all HPs through wandb.config, so logging matches execution!
+        config = wandb.config
+
+        # make the model, data, and optimization problem
+        model, test_dataloader = make(config)
+        # print(model)
+
+        # and use them to train the model
+        model = inference_loop(
+            model_type=config.model_type,
+            model=model,
+            batch_size=config.batch_size,
+            test_dataloader=test_dataloader,
+            snr_min=config.snr_min,
+            snr_max=config.snr_max,
+            delay_max=config.delay_max,
+        )
+
+    return model
+
+
+if __name__ == "__main__":
+
+    # Ensure deterministic behavior
+    torch.backends.cudnn.deterministic = True
+    random.seed(hash("setting random seeds") % 2**32 - 1)
+    np.random.seed(hash("improves reproducibility") % 2**32 - 1)
+    torch.manual_seed(hash("by removing stochasticity") % 2**32 - 1)
+    torch.cuda.manual_seed_all(hash("so runs are repeatable") % 2**32 - 1)
+
+    # Initialize Weights and Biases
+    wandb.login()
+
+    # defining model save location
+    save_model_folder = "CNN_AutoEncoder_20240917_11_29_24"
+
+    config = dict(
+        model_type="CNN_AutoEncoder",
+        description="inference CNN AutoEncoder with Joint Syncronization, Equalization, and Channel Decoding",
+        save_model_folder=save_model_folder,  # the folder that the model be saved
+        model_file=f"./models/{save_model_folder}/{save_model_folder + '_epoch75'}.pth",
+        batch_size=512,
+        learning_rate=1e-3,
+        M1=300,
+        M2=200,
+        k=80,  # number of information bits
+        N=288,  # number of coded bits
+        L=16,  # L is the greatest common divisor of N and K
+        k_prime=4,  # k = K_prime * L
+        N_prime=18,  # N = N_prime * L
+        n=144,  # number of complex channel uses
+        k_mod=2,  # number of bits per symbol
+        F=20,  # info feature size, indicating the number of info exchanged between  EQ-CNN and DEC-CNN
+        delay_max=10,  # maximum delay units in the channel
+        nb=4,  # number of fading blocks
+        ts=1,  # sampling interval
+        N_up=5,  # upsampling rate
+        tp=5,  # period of the pulse
+        snr_min=2,
+        snr_max=20,
+        alpha=0.01,  # factor of the sync loss in the total loss
+    )
+
+    # Build, train and analyze the model with the pipeline
+    model = model_pipeline(config)
---
+++ src/inference/inference_utils_sync_equ.py
@@ -0,0 +1,160 @@
+"""
+This files contains the gengeral-purpose functions to train the model.
+
+"""
+
+import torch
+import wandb
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from features.build_pytorch_dataset import InfobitDataset
+from train.train_utils_sync_equ import generate_random_delay
+
+
+def inference_loop(
+    model_type,
+    model,
+    batch_size,
+    test_dataloader,
+    test_snr_min,
+    test_snr_max,
+    delay_max,
+):
+    """
+    inference loop for the well trained model.
+
+
+    """
+
+    print("Starting inference ...")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    # inference snr_db values is a list from test_snr_min to test_snr_max with step 0.5, contains 2 sides of the SNR_db
+    inference_snr_db = torch.arange(test_snr_min, test_snr_max + 0.5, 0.5)
+
+    # initialize the list to store the bit error rate and block error rate
+    infer_ber_list = []  # list to store the bit error rate
+    infer_bler_list = []  # list to store the block error rate
+
+    # switch to evaluation mode
+    model.eval()
+
+    # turn off gradients
+    with torch.no_grad():
+
+        for i in range(len(inference_snr_db)):
+
+            # initialize the list to store the bit error rate and block error rate
+            ber_snr_i = []  # list to store the bit error rate
+            bler_snr_i = []  # list to store the block error rate
+
+            for batch_index, data in enumerate(test_dataloader, 1):
+
+                # randomly generate delay for each message
+                # delay has the size (batch_size, 1),
+                # delay_onehot has the size (batch_size, 1, delay_max + 1)
+                true_delay, true_delay_onehot = generate_random_delay(
+                    batch_size=batch_size, delay_max=delay_max, device=device
+                )
+
+                # get the features and targets, X has shape (batch_size, 1, k)
+                X = data.to(device)
+
+                # prediction by the model, shape (batch_size, 1, k)
+                estimated_delay, predictions = model.forward(
+                    X,
+                    true_delay,
+                    true_delay_onehot,
+                    SNR_db=inference_snr_db[i],
+                    device=device,
+                    training=False,
+                )
+
+                # round the predictions to 0 or 1
+                predictions_round = torch.round(predictions)
+
+                # compute the bit error rate and block error rate
+                infer_ber_batch, infer_bler_batch = compute_ber_bler(
+                    predictions_round, X
+                )
+
+                # append the bit error rate and block error rate to the list
+                ber_snr_i.append(infer_ber_batch)
+                bler_snr_i.append(infer_bler_batch)
+
+            # compute the average bit error rate and block error rate
+            infer_ber_avg = sum(ber_snr_i) / len(ber_snr_i)
+            infer_bler_avg = sum(bler_snr_i) / len(bler_snr_i)
+
+            # append the average bit error rate and block error rate to the list
+            infer_ber_list.append(infer_ber_avg)
+            infer_bler_list.append(infer_bler_avg)
+
+    # define our custom x axis metric
+    wandb.define_metric("custom_step")
+
+    # define which metrics will be plotted against it
+    # first plot for BER
+    wandb.define_metric(
+        f"BER v.s. SNR (Joint Sync and Decoding, Block Fading Channel)",
+        step_metric="custom_step",
+    )
+
+    # second plot for BLER
+    wandb.define_metric(
+        f"BLER v.s. SNR (Joint Sync and Decoding, Block Fading Channel)",
+        step_metric="custom_step",
+    )
+
+    for i in range(len(inference_snr_db)):
+
+        log_dict = {
+            f"BER v.s. SNR (Joint Sync and Decoding, Block Fading Channel)": infer_ber_list[
+                i
+            ],
+            "custom_step": inference_snr_db[i],
+        }
+        wandb.log(log_dict)
+
+        log_dict = {
+            f"BLER v.s. SNR (Joint Sync and Decoding, Block Fading Channel)": infer_bler_list[
+                i
+            ],
+            "custom_step": inference_snr_db[i],
+        }
+        wandb.log(log_dict)
+
+    print("The snr_db values for inference are: ", inference_snr_db)
+    print("The average bit error rate is: ", infer_ber_list)
+    print("The average block error rate is: ", infer_bler_list)
+
+    print("Finished inference!")
+
+    return model
+
+
+def compute_ber_bler(predictions, targets):
+    """
+    Compute the bit error rate and block error rate.
+
+    Args:
+        predictions (torch.Tensor): The predicted values of shape (batch_size, 1, k).
+        targets (torch.Tensor): The target values of shape (batch_size, 1, k).
+
+    Returns:
+        float: The bit error rate.
+        float: The block error rate.
+    """
+    # compute the bit error rate
+    ber = torch.sum(torch.abs(predictions - targets)) / (
+        targets.shape[0] * targets.shape[2]
+    )
+
+    # compute the block error rate
+    bler = (
+        torch.sum(torch.any(torch.abs(predictions - targets), dim=2)) / targets.shape[0]
+    )
+
+    return ber, bler
