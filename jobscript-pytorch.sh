d18acf436afee58621c59eb1ffd488c1d9f2b587
Bingcheng Chen <chenbin@chalmers.se>
2024-09-24 11:06:53 +0200

    Update jobscript-pytorch.sh for GPU configuration and inference script

--- jobscript-pytorch.sh
+++ jobscript-pytorch.sh
@@ -2,14 +2,14 @@
 
 #SBATCH -A NAISS2024-5-119  # find your project with the "projinfo" command
 #SBATCH -p alvis
-#SBATCH -t 00:20:00  # hours:minutes:seconds
-#SBATCH --gpus-per-node=A40:1
-#SBATCH -J "Test"
+#SBATCH -t 20:00:00  # hours:minutes:seconds
+#SBATCH --gpus-per-node=A100:1
+#SBATCH -J "CNN_AutoEncoder_long"
 
 # Set-up environment
 ml purge
-# module load PyTorch-bundle/1.12.1-foss-2022a-CUDA-11.7.0
-module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
+module load PyTorch-bundle/1.12.1-foss-2022a-CUDA-11.7.0
+# module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
 module load wandb/0.13.4-GCCcore-11.3.0
 
 
@@ -25,3 +25,5 @@ module load wandb/0.13.4-GCCcore-11.3.0
 # or you can instead use
 #jupyter nbconvert --to python data-pytorch.ipynb &&
 python3 ./src/train/train_CNN_AutoEncoder_AWGN.py
+# python3 ./src/inference/inference_CNN_AutoEncoder_AWGN.py
+
